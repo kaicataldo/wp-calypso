7e0f468b6701af8e6460ccf34641e04689d1d668
Veselin Nikolov <veselin@automattic.com>
2019-07-31 16:35:01 +0300

    Correctly handle the situation after the refund period is over

--- client/components/marketing-survey/cancel-purchase-form/index.jsx
+++ client/components/marketing-survey/cancel-purchase-form/index.jsx
@@ -36,7 +36,7 @@ import BusinessATStep from './step-components/business-at-step';
 import UpgradeATStep from './step-components/upgrade-at-step';
 import PrecancellationChatButton from './precancellation-chat-button';
 import DowngradeStep from './step-components/downgrade-step';
-import { getName } from 'lib/purchases';
+import { getName, isRefundable } from 'lib/purchases';
 import { isGoogleApps } from 'lib/products-values';
 import { radioOption } from './radio-option';
 import {
@@ -527,6 +527,11 @@ class CancelPurchaseForm extends React.Component {
 	getRefundAmount = () => {
 		const { purchase, downgradePlanPrice, includedDomainPurchase } = this.props;
 		const { precision } = getCurrencyDefaults( purchase.currencyCode );
+
+		if ( ! isRefundable( purchase ) ) {
+			return 0;
+		}
+
 		const refundAmount =
 			purchase.refundAmount +
 			( includedDomainPurchase ? includedDomainPurchase.costToUnbundle : 0 ) -
@@ -536,11 +541,11 @@ class CancelPurchaseForm extends React.Component {
 			return 0;
 		}
 
-		return purchase.currencySymbol + parseFloat( refundAmount ).toFixed( precision );
+		return parseFloat( refundAmount ).toFixed( precision );
 	};
 
 	surveyContent() {
-		const { translate, isImport, showSurvey } = this.props;
+		const { translate, isImport, showSurvey, purchase } = this.props;
 		const { surveyStep } = this.state;
 
 		if ( showSurvey ) {
@@ -595,6 +600,7 @@ class CancelPurchaseForm extends React.Component {
 			if ( surveyStep === steps.DOWNGRADE_STEP ) {
 				return (
 					<DowngradeStep
+						currencySymbol={ purchase.currencySymbol }
 						planCost={ this.props.downgradePlanPrice }
 						refundAmount={ this.getRefundAmount() }
 					/>
